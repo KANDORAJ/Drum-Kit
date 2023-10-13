2b17d2196366cb7d040725577c1d6669188af1f5
Ayberk <ayberk_kandemir@aof.anadolu.edu.tr>
2023-10-13 04:17:23 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,69 @@
+//Detecting Button Press
+
+var numberOfDrumButtons = document.querySelectorAll(".drum").length;
+
+for (var i = 0; i < numberOfDrumButtons; i++) {
+    document.querySelectorAll(".drum")[i].addEventListener("click", function(){
+        var buttonInnerHTML = this.innerHTML;
+        makeSound(buttonInnerHTML);
+        buttonAnimation(buttonInnerHTML);
+    });
+};
+
+//Detecting Keyboard Press 
+document.addEventListener("keydown", function(event){
+    makeSound(event.key.toLowerCase());
+    buttonAnimation(event.key.toLowerCase());
+    }
+);
+
+/**
+ * The function `makeSound` plays a specific sound based on the key input.
+ * @param key - The key parameter represents the key that was pressed on the keyboard. It is used to
+ * determine which sound to play based on the key pressed.
+ */
+function makeSound(key){
+    switch (key) {
+        case "w":
+            var snare = new Audio("/sounds/snare.mp3");
+            snare.play();
+            break;
+        case "a":
+            var tom1 = new Audio("/sounds/tom-1.mp3");
+            tom1.play();
+            break;
+        case "s":
+            var tom3 = new Audio("/sounds/tom-3.mp3");
+            tom3.play();
+            break;
+        case "d":
+            var kick = new Audio("/sounds/kick-bass.mp3");
+            kick.play();
+            break;
+        case "j":
+            var tom4 = new Audio("/sounds/tom-4.mp3");
+            tom4.play();
+            break;
+        case "k":
+            var tom2 = new Audio("/sounds/tom-2.mp3");
+            tom2.play();
+            break;
+        case "l":
+            var crash = new Audio("/sounds/crash.mp3");
+            crash.play();
+            break;
+        
+        default: console.log(buttonInnerHTML);
+}
+};
+
+// var audio1 = new Audio("/sounds/tom-1.mp3");
+// audio1.play();
+
+function buttonAnimation(currentKey){
+    var activeButton = document.querySelector("." + currentKey);
+    activeButton.classList.add("pressed");
+    setTimeout(() => {
+        activeButton.classList.remove("pressed");
+    }, 100);
+}
